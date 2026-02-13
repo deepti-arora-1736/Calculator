24cacda90161f2faf3f87628da7468c399ee9540
Deepti Arora <deeparora1736@gmail.com>
2026-02-13 11:11:51 +0530

    double to Double

--- src/main/java/org/deepti/calculator/controller/CalculatorController.java
+++ src/main/java/org/deepti/calculator/controller/CalculatorController.java
@@ -20,36 +20,36 @@ public class CalculatorController {
     }
 
     @GetMapping("/add")
-    public ResponseEntity<ResponseDTO> add(@RequestParam double a, @RequestParam double b){
+    public ResponseEntity<ResponseDTO> add(@RequestParam Double a, @RequestParam Double b){
         log.info("Adding {} {}",a,b);
-        double result=calculatorService.add(a,b);
+        Double result=calculatorService.add(a,b);
         log.info("Added Successfully");
         ResponseDTO response=new ResponseDTO(result,"Successful",true);
         return new ResponseEntity<>(response,HttpStatus.OK);
     }
 
     @GetMapping("/subtract")
-    public ResponseEntity<ResponseDTO> subtract(@RequestParam double a, @RequestParam double b){
+    public ResponseEntity<ResponseDTO> subtract(@RequestParam Double a, @RequestParam Double b){
         log.info("Subtracting {} {}",a,b);
-        double result=calculatorService.subtract(a,b);
+        Double result=calculatorService.subtract(a,b);
         log.info("Subtracted Successfully");
         ResponseDTO response=new ResponseDTO(result,"Successful",true);
         return new ResponseEntity<>(response,HttpStatus.OK);
     }
 
     @GetMapping("/multiply")
-    public ResponseEntity<ResponseDTO> multiply(@RequestParam double a, @RequestParam double b){
+    public ResponseEntity<ResponseDTO> multiply(@RequestParam Double a, @RequestParam Double b){
         log.info("Multiplying {} {}",a,b);
-        double result=calculatorService.multiply(a,b);
+        Double result=calculatorService.multiply(a,b);
         log.info("Multiplication Successful");
         ResponseDTO response=new ResponseDTO(result,"Successful",true);
         return new ResponseEntity<>(response,HttpStatus.OK);
     }
 
     @GetMapping("/divide")
-    public ResponseEntity<ResponseDTO> divide(@RequestParam double a, @RequestParam double b){
+    public ResponseEntity<ResponseDTO> divide(@RequestParam Double a, @RequestParam Double b){
         log.info("Dividing {} {}",a,b);
-        double result=calculatorService.divide(a,b);
+        Double result=calculatorService.divide(a,b);
         log.info("Divided Successfully");
         ResponseDTO response=new ResponseDTO(result,"Successful",true);
         return new ResponseEntity<>(response,HttpStatus.OK);
